293eecbfcebc9bd8dd6896ce8e1fd0b894f2a5a9
Matheus <matheusvitor8143@gmail.com>
2020-09-14 22:10:09 -0300

    terminado

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
-import 'TodoList.dart';
-
+import 'todo_list.dart';
 void main() {
+
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'TodoList',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.blue,
-      ),
-
+      title: 'Todos',
       home: TodoList(),
     );
   }
