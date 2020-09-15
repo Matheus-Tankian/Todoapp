21fb817d27eb2fdbaf475ce671415b10c5bf59dd
Matheus Vitor de Oliveira Cruz <61215783+Matheus-Tankian@users.noreply.github.com>
2020-09-14 22:12:33 -0300

    Update database_helper.dart

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -8,8 +8,8 @@ import 'package:todolist/todo.dart';
 class DatabaseHelper {
 
 
-  static DatabaseHelper _databaseHelper; // Singleton DatabaseHelper
-  static Database _database; // Singleton Database
+  static DatabaseHelper _databaseHelper; 
+  static Database _database; 
 
 
   String todoTable = 'todo_table';
@@ -19,13 +19,12 @@ class DatabaseHelper {
   String colDate = 'date';
 
 
-  DatabaseHelper._createInstance(); // Named constructor to create instance of DatabaseHelper
-
+  DatabaseHelper._createInstance(); 
 
   factory DatabaseHelper() {
     if (_databaseHelper == null) {
       _databaseHelper = DatabaseHelper
-          ._createInstance(); // This is executed only once, singleton object
+          ._createInstance(); 
     }
     return _databaseHelper;
   }
@@ -40,12 +39,12 @@ class DatabaseHelper {
 
 
   Future<Database> initializeDatabase() async {
-    // Get the directory path for both Android and iOS to store database.
+   
     Directory directory = await getApplicationDocumentsDirectory();
     String path = directory.path + 'todos.db';
 
 
-    // Open/create the database at a given path
+    
     var todosDatabase = await openDatabase(
         path, version: 1, onCreate: _createDb);
     return todosDatabase;
@@ -59,18 +58,18 @@ class DatabaseHelper {
   }
 
 
-  // Fetch Operation: Get all todo objects from database
+  
   Future<List<Map<String, dynamic>>> getTodoMapList() async {
     Database db = await this.database;
 
 
-    //		var result = await db.rawQuery('SELECT * FROM $todoTable order by $colTitle ASC');
+   
     var result = await db.query(todoTable, orderBy: '$colTitle ASC');
     return result;
   }
 
 
-  // Insert Operation: Insert a todo object to database
+ 
   Future<int> insertTodo(Todo todo) async {
     Database db = await this.database;
     var result = await db.insert(todoTable, todo.toMap());
@@ -78,7 +77,7 @@ class DatabaseHelper {
   }
 
 
-  // Update Operation: Update a todo object and save it to database
+  
   Future<int> updateTodo(Todo todo) async {
     var db = await this.database;
     var result = await db.update(
@@ -87,7 +86,7 @@ class DatabaseHelper {
   }
 
 
-  // Delete Operation: Delete a todo object from database
+  
   Future<int> deleteTodo(int id) async {
     var db = await this.database;
     int result = await db.rawDelete(
@@ -96,7 +95,7 @@ class DatabaseHelper {
   }
 
 
-  // Get number of todo objects in database
+ 
   Future<int> getCount() async {
     Database db = await this.database;
     List<Map<String, dynamic>> x = await db.rawQuery(
@@ -106,15 +105,15 @@ class DatabaseHelper {
   }
 
 
-  // Get the 'Map List' [ List<Map> ] and convert it to 'todo List' [ List<Todo> ]
+ 
   Future<List<Todo>> getTodoList() async {
-    var todoMapList = await getTodoMapList(); // Get 'Map List' from database
+    var todoMapList = await getTodoMapList(); 
     int count = todoMapList
-        .length; // Count the number of map entries in db table
+        .length; 
 
 
     List<Todo> todoList = List<Todo>();
-    // For loop to create a 'todo List' from a 'Map List'
+   
     for (int i = 0; i < count; i++) {
       todoList.add(Todo.fromMapObject(todoMapList[i]));
     }
